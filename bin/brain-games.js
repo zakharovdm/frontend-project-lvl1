80c7a5e82084ba6f9d1d045665d3c456562f2b60
Dmitry Zakharov <dzaharov794@gmail.com>
2021-12-27 08:25:35 +0300

    Add function greeting user

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,5 @@
 #!/usr/bin/env node
+
+import { greetingsUser } from '../src/cli.js';
+
+greetingsUser();
---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const greetingsUser = () => {
+  console.log('Welcome to the Brain Games!');
+  const nameUser = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${nameUser}`);
+}
+
+export { greetingsUser };
