029bcb1ddef9992873c67b8dcaf6d8c4a4df0e11
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-01-09 23:16:55 +0300

    Fix lint errors

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -1,12 +1,10 @@
 import startGameLogic from '../index.js';
 import getRandomNumber from '../get-random.js';
 
-const rules = "Find the greatest common divisor of given numbers.";
+const rules = 'Find the greatest common divisor of given numbers.';
 
 const LOWERBOUND = 0;
 const UPPERBOUND = 100;
-
-const startGame = () => startGameLogic(rules, generateQuestionAnswer);
 
 const findGcd = (firstValue, secondValue) => {
   if (firstValue === secondValue) {
@@ -16,22 +14,24 @@ const findGcd = (firstValue, secondValue) => {
   let greaterValue = Math.max(firstValue, secondValue);
   let lesserValue = Math.min(firstValue, secondValue);
 
-  while (greaterValue % lesserValue != 0) {
-    let residue = greaterValue % lesserValue;
+  while (greaterValue % lesserValue !== 0) {
+    const residue = greaterValue % lesserValue;
     greaterValue = lesserValue;
     lesserValue = residue;
   }
-  let gcd = lesserValue;
+  const gcd = lesserValue;
   return gcd;
 };
-    
+
 const generateQuestionAnswer = () => {
   const firstNumber = getRandomNumber(LOWERBOUND, UPPERBOUND);
   const secondNumber = getRandomNumber(LOWERBOUND, UPPERBOUND);
-  
+
   const question = `${firstNumber} ${secondNumber}`;
   const answer = findGcd(firstNumber, secondNumber);
   return [question, answer];
 };
+
+const startGame = () => startGameLogic(rules, generateQuestionAnswer);
 
 export default startGame;
